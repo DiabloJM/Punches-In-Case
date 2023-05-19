e1dda5d846a99b999744045190f503b2ce6399a8
DiabloJM <fernandojmichel@outlook.com>
2023-05-19 12:43:05 -0600

    fix experience bug
    add level structure and modify style

--- Assets/Scripts/ExperienceSystem/ExperienceAndLevel.cs
+++ Assets/Scripts/ExperienceSystem/ExperienceAndLevel.cs
@@ -1,67 +1,44 @@
 using UnityEngine;
 
-//Clase que implementan las interfaces para tener manejo de la experiencia y nivel 
 public class ExperienceAndLevel : MonoBehaviour, IExperience, ILevel
 {
-    [SerializeField] public int m_Experience = 0; //Cantidad de experiencia
-    [SerializeField] private int m_Level = 0; //Nivel actual del jugador
-    [SerializeField] public int m_ExperiencePerLevel; //Experiencia que necesita el jugador para subir de nivel
-    [SerializeField] private int m_MultiplierPerLevel; //Multiplicador por nivel
+    [SerializeField] public int m_Experience = 0;
+    [SerializeField] private int m_Level = 0;
+    [SerializeField] public int m_ExperiencePerLevel;
+    [SerializeField] private int m_MultiplierPerLevel;
 
     public int Experience => m_Experience;
     public int Level => m_Level;
 
     private void Update()
     {
-        //Metodo para probar el sistema
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_Experience += 50;
-
-            AddExperience(m_Experience);
+            AddExperience(50);
         }
     }
 
     public void AddExperience(int experience)
     {
-        //Asegurarse de que sea un valor positivo
         experience = Mathf.Abs(experience);
 
-        //Almacenar la experiencia temporalmente para realizar operaciones sobre ella
+        int experienceToUpgrade = m_ExperiencePerLevel * m_MultiplierPerLevel;
+
         int remainingExperience = experience;
-
-        /*do
+        while (remainingExperience >= experienceToUpgrade)
         {
-            //Calcula la cantidad maxima de experiencia necesaria para cambiar de nivel
-            int experienceToUpgrade = m_ExperiencePerLevel * m_MultiplierPerLevel;
+            AddLevel(1);
+            remainingExperience -= experienceToUpgrade;
+            experienceToUpgrade = m_ExperiencePerLevel * m_MultiplierPerLevel;
+        }
 
-            //Si la cantidad de experiencia a agregar hace que suba de nivel
-            if (m_Experience + remainingExperience >= experienceToUpgrade)
-            {
-                //Añade un nivel de experiencia
-                AddLevel(1);
-
-                //Calcular la cantidad de experiencia agregada y la resta de la experiencia temporal
-                int added = experienceToUpgrade - m_Experience;
-                remainingExperience -= added;
-            }
-            else
-            {
-                //Asignar la experiencia sobrante y terminar el while
-                m_Experience = remainingExperience;
-                remainingExperience = 0;
-            }
-        } while (remainingExperience > 0);*/
+        m_Experience += remainingExperience;
     }
 
-    //Funcion para sumar un nivel al jugador
     public void AddLevel(int level)
     {
-        //Evitar que sea negativo y agregar experiencia
         level = Mathf.Abs(level);
         m_Level += level;
-
-        //Formula para calcular el incremento de experiencia necesaria para subir de nivel
-        m_ExperiencePerLevel = m_ExperiencePerLevel * m_MultiplierPerLevel;
+        m_ExperiencePerLevel *= m_MultiplierPerLevel;
     }
-} 
+}
