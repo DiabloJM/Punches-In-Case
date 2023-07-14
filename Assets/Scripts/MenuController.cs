fa2db0f6341c22714e6e2b37d1db7aa2f380cc52
WVDante16 <walbertozekrom@gmail.com>
2023-07-13 19:43:46 -0600

    MainMenu y pausa

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,18 +8,45 @@ public class MenuController : MonoBehaviour
     //Funcion para ir al nivel 1
     public void Level1()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("TheCity");
     }
 
     //Funcion para ir al nivel 2
     public void Level2()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("TheBridge");
     }
 
     //Funcion para ir al nivel 3
     public void Level3()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("TheDocks");
+    }
+
+    //Funcion para salir del juego
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    //Funcion para pausar el juego
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    //Funcion para reanudar el juego
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+    //Funcion para regresar al menu
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
