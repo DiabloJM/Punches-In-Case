9b25e1b3e7191cbe234a3c54ad2c658e20f93575
GilbertoSSJ <113067525+GilbertoSSJ@users.noreply.github.com>
2023-07-27 10:16:30 -0600

    Player2dMovementBase

---
+++ Assets/PlayerMovement2d.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2d : MonoBehaviour
+{
+    public Animator animator; // Variable para almacenar la referencia al componente Animator
+    public float movementSpeed = 5f;
+    public int vidaPlayer = 50;
+
+    string CrescentKick2dParameter = "CrescentKick2d";
+    string EnemyHit2dParameter = "EnemyHit";
+    string Run2d = "Run";
+
+    private Vector3 movementDirection;
+    private bool isRunning;
+
+    private void Start()
+    {
+        SetPlayerScale(1f);
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Mover al jugador en la dirección calculada
+        movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        transform.Translate(movementDirection * movementSpeed * Time.deltaTime);
+
+        // Reproducir la animación al hacer clic izquierdo
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool(CrescentKick2dParameter, true);
+        }
+
+        // Controlar la animación de correr
+        isRunning = Mathf.Abs(horizontalInput) > 0.1f;
+        animator.SetBool(Run2d, isRunning);
+
+     
+
+
+        // Cambiar la escala según la dirección
+        if (horizontalInput < 0f)
+        {
+            SetPlayerScale(-1f);
+        }
+        else if (horizontalInput > 0f)
+        {
+            SetPlayerScale(1f);
+        }
+    }
+
+    // Función para cambiar el local scale del jugador
+    private void SetPlayerScale(float scaleX)
+    {
+        transform.localScale = new Vector3(scaleX, 1f, 1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("Auchhh");
+            animator.SetBool(EnemyHit2dParameter, true);
+            vidaPlayer -= 25;
+        }
+    }
+}
